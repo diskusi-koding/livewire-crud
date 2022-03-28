668166c70a398898fe96cacc84bd2f8832f03356
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-03-28 15:21:54 +0700

    Create ContactSeeder.php

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*1. perintah untuk menjalankan factory model Contact*/
+
+        Contact::factory()->count(50)->create();
+    }
+}
