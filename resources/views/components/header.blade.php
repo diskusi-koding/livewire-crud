abb3c9b4955e9ff277acbbab11d3028172b6999c
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-03-28 15:21:59 +0700

    Create header.blade.php

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom border-light fixed">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">MyContact</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
