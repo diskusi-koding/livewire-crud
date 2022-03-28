e123e90fefce11ab8381c77055bbdd7773b2eff4
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-03-28 15:22:06 +0700

    Create index.blade.php

---
+++ resources/views/livewire/contact/index.blade.php
@@ -0,0 +1,27 @@
+<div>
+    {{-- Stop trying to control. --}}
+    <table class="table table-hover">
+        <thead class="bg-light">
+        <tr>
+            <th>Nama</th>
+            <th>Telepon</th>
+            <th>Email</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse($contacts as $contact)
+            <tr>
+                <td>{{ $contact->nama_lengkap }}</td>
+                <td>{{ $contact->telepon }}</td>
+                <td>{{ $contact->email }}</td>
+                <td>{{$contact->status}}</td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="3" class="text-center">Tidak ada data</td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
+</div>
