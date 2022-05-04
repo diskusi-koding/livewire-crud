a62b1011310f0ee1d3b2f8253c3fcee5a61ef55e
Abd. Asis <id.abdasis@gmail.com>
2022-05-04 14:40:33 +0700

    Perbaikan pada index

--- resources/views/livewire/contact/index.blade.php
+++ resources/views/livewire/contact/index.blade.php
@@ -2,26 +2,26 @@
     {{-- Stop trying to control. --}}
     <table class="table table-hover">
         <thead class="bg-light">
-        <tr>
-            <th>Nama</th>
-            <th>Telepon</th>
-            <th>Email</th>
-            <th>Status</th>
-        </tr>
+            <tr>
+                <th>Nama</th>
+                <th>Telepon</th>
+                <th>Email</th>
+                <th>Status</th>
+            </tr>
         </thead>
         <tbody>
-        @forelse($contacts as $contact)
+            @forelse($contacts as $contact)
             <tr>
                 <td>{{ $contact->nama_lengkap }}</td>
                 <td>{{ $contact->telepon }}</td>
                 <td>{{ $contact->email }}</td>
                 <td>{{$contact->status}}</td>
             </tr>
-        @empty
+            @empty
             <tr>
-                <td colspan="3" class="text-center">Tidak ada data</td>
+                <td colspan="4" class="text-center">Tidak ada data</td>
             </tr>
-        @endforelse
+            @endforelse
         </tbody>
     </table>
 </div>
